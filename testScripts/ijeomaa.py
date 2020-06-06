4736ba480c404961315285fa2b06d611247b02d7
ijeomaa <66265814+ijeomaa@users.noreply.github.com>
2020-06-06 22:31:59 +0100

    script corrected, should pass now
    I just added the square brackets, didn't know of that before

--- testScripts/ijeomaa.py
+++ testScripts/ijeomaa.py
@@ -4,4 +4,4 @@ hng_id = 'HNG-04396'
 language = 'Python'
 email = 'Ijaygift@gmail.com'
 
-print("Hello World, this is %s %s with HNGi7 ID %s using %s for stage 2 task. %s"%(firstname, lastname, hng_id, language, email))
+print("Hello World, this is [%s] [%s] with HNGi7 ID [%s] using [%s] for stage 2 task. %s"%(firstname, lastname, hng_id, language, email))
